7bfc2d221a5a2215ea2ae8a00c54eb154b8cfd73
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-14 13:04:59 -0300

    footer adicionado no layout global

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+        <div>eu sou um footer</div>
+    )
+}
--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import Navbar from "./Navbar"
 
 export default function MainContainer({children}){
@@ -5,6 +6,7 @@ export default function MainContainer({children}){
         <>
             <Navbar/>
             <div>{children}</div>
+            <Footer/>
         </>
     )
 }
