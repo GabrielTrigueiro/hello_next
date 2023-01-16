82ffc9d4d2684b41df229a668dfbdc767c71d374
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-16 18:51:15 -0300

    rotas dinamicas com dados dinamicos

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import styles from "../../styles/Todos.module.css"
 
 export async function getStaticProps() {
@@ -5,7 +6,9 @@ export async function getStaticProps() {
     const data = await fetch('https://jsonplaceholder.typicode.com/todos')
 
     const todos = await data.json()
+
     console.log(todos)
+
     return{
         props: {todos}
     }
@@ -17,8 +20,8 @@ export default function Todos({todos}) {
             <h1>Tarefas a serem feitas:</h1>
             <ul className={styles.todolist}>
                 {todos.map((item) => (
-                    <li key={item.id}>
-                        {item.title}
+                    <li key={item.id}>  
+                        {item.title} - <Link href={`/todos/${item.id}`}>Ver mais</Link>
                     </li>
                 ))}
             </ul>
